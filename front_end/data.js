e467863fdc00c5f80b41836f15170734a2e44449
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-05-03 20:27:40 +0300

    portfolio

---
+++ front_end/data.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: '/assets/music/song 1.mp3',
+        artist:'artist 1',
+        cover:'/assets/images/cover 1.mp3'
+    },
+    {
+        name: 'song 2',
+        path: '/assets/music/song 2.mp3',
+        artist:'artist 2',
+        cover:'/assets/images/cover 2.mp3'
+    },
+    {
+        name: 'song 3',
+        path: '/assets/music/song 3.mp3',
+        artist:'artist 3',
+        cover:'/assets/images/cover 3.mp3'
+    },
+    {
+        name: 'song 4',
+        path: '/assets/music/song 4.mp3',
+        artist:'artist 4',
+        cover:'/assets/images/cover 4.mp3'
+    },
+    {
+        name: 'song 5',
+        path: '/assets/music/song 5.mp3',
+        artist:'artist 5',
+        cover:'/assets/images/cover 5.mp3'
+    },
+    {
+        name: 'song 6',
+        path: '/assets/music/song 6.mp3',
+        artist:'artist 6',
+        cover:'/assets/images/cover 6.mp3'
+    },
+    {
+        name: 'song 7',
+        path: '/assets/music/song 7.mp3',
+        artist:'artist 7',
+        cover:'/assets/images/cover 7.mp3'
+    }
+]
